6a9f55920236b74ad24440f85325ed2eb6b6eabb
Niraj-Thorat <thoratniru@gmail.com>
2024-04-11 20:05:26 +0530

    Qualifiers

--- src/main/java/com/example/MyApp/rest/DemoController.java
+++ src/main/java/com/example/MyApp/rest/DemoController.java
@@ -1,6 +1,7 @@
 package com.example.MyApp.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,24 +9,24 @@ import com.example.MyApp.common.Coach;
 
 @RestController
 public class DemoController {
-    //Define private field for dependecncy
-    //@Autowired
+    // Define private field for dependecncy
+    // @Autowired
     private Coach myCoach;
 
-    //Define constructor for dependency injection
-    // @Autowired
-    // public DemoController(Coach theCoach){
-    //     myCoach = theCoach;
-    // }
-
-    //Setter Injection
+    // Define constructor for dependency injection
     @Autowired
-    public void setCoach(Coach theCoach){
+    public DemoController(@Qualifier("trackCoach") Coach theCoach) {
         myCoach = theCoach;
     }
 
+    // Setter Injection
+    // @Autowired
+    // public void setCoach(@Qualifier("baseballCoach") Coach theCoach){
+    // myCoach = theCoach;
+    // }
+
     @GetMapping("/dailyworkout")
-    public String getDailyWorkout(){
+    public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
     }
 }
