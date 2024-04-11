a1da5081000588b80528f311adffc33647029f63
Niraj-Thorat <thoratniru@gmail.com>
2024-04-11 18:44:24 +0530

    Custom Application Properties

--- src/main/java/com/example/MyApp/rest/TestController.java
+++ src/main/java/com/example/MyApp/rest/TestController.java
@@ -1,13 +1,26 @@
 package com.example.MyApp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${dev.name}")
+    private String userName;
+    
+    @Value("${dev.role}")
+    private String userRole;
+
     @GetMapping("/")
     public String sayHello(){
-        return "Hello World";
+        return "Hello World!";
+    }
+
+    @GetMapping("/userinfo")
+    public String getUserInfo(){
+        return "Username: " + userName +"; Role:"+ userRole;
     }
 
 }
