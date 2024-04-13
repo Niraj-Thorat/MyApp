a5659a7d9957f6c6d0864e95065d72312ce53892
Niraj-Thorat <thoratniru@gmail.com>
2024-04-13 09:49:09 +0530

    Beans Lifecycle Methods

--- src/main/java/com/example/MyApp/common/TrackCoach.java
+++ src/main/java/com/example/MyApp/common/TrackCoach.java
@@ -4,12 +4,27 @@ import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class TrackCoach implements Coach {
 
     public TrackCoach() {
         System.out.println("In Constructor: " + getClass().getSimpleName());
+    }
+
+    // define init method 
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println("In doMyStartupStuff() : "+ getClass().getSimpleName());
+    }
+
+    // define destroy method - destroy method wont be called for prototype scoped beans
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println("In doMyCleanupStuff() : "+ getClass().getSimpleName());
     }
 
     @Override
